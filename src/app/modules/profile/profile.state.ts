62f2a1eb50156fa4137a2f40965ebede50a5adbd
dll <greenyourcode@gmail.com>
2020-05-13 15:48:28 +0200

    step8

--- src/app/modules/profile/profile.state.ts
+++ src/app/modules/profile/profile.state.ts
@@ -1,10 +1,11 @@
-export class ProfileState {
-  name: string;
-  id: string;
-  settings: ProfileSettingsState;
-}
-
 export class ProfileSettingsState {
   privateProfile: string;
   themeColor = 'green';
 }
+
+export class ProfileState {
+  name: string;
+  id: string;
+  settings = new ProfileSettingsState();
+}
+
